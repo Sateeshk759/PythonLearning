d176a5ef8f8a33ade8d93e57a5465fb91aabc71c
Sateeshk759 <sateesh.k759@gmail.com>
2025-04-12 10:51:51 +0530

    subtraction of two numbers

---
+++ subtract.py
@@ -0,0 +1,16 @@
+def Subtractnumbers(num1, num2):
+    """
+    Subtract two numbers and return the result.
+    
+    :param num1: First number
+    :param num2: Second number
+    :return: The result of num1 - num2
+    """
+    return num1 - num2  
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))                    
+result = Subtractnumbers(num1, num2)
+print(f"The difference of {num1} and {num2} is {result}")   
+
+
+
